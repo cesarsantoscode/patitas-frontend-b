14ef915b4a14d9be4506657a6cd26f6c3cc28a0b
César Santos <cesarsantostorres@gmail.com>
2024-09-15 16:15:01 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_b/PatitasFrontendBApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_frontend_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasFrontendBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasFrontendBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/patitas_frontend_b/PatitasFrontendBApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_frontend_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasFrontendBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
